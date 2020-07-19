ad28263e76cd309b93b375f6a3dfbb802f0d135b
Alexander Brown <alexmb123@gmail.com>
2020-07-19 14:24:41 -0500

    connections

--- db/connections.js
+++ db/connections.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/iceCream', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/iceCream',  
+{ 
+    useUnifiedTopology: true, 
+    useNewUrlParser: true, 
+    useFindAndModify: false  
+})
 
 mongoose.Promise = Promise
 
