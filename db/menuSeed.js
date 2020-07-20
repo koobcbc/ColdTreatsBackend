efb736839320daa16b53141bb5ad1f284f79ffb0
Alexander Brown <alexmb123@gmail.com>
2020-07-20 18:01:34 -0500

    updated new Schema

--- db/menuSeed.js
+++ db/menuSeed.js
@@ -10,9 +10,38 @@ const main = async () => {
   
     const menu = [
         {
-            flavor: ['Chocolate', 'Vanilla', 'Strawberry', 'Cookies n Cream', 'Mint Chocolate Chip'],
-            toppings: ['Oroes', 'Sprinkles', 'Gummy Bears'],
-            size: ['small', 'medium', 'large']
+            flavor: [
+                {flavor1:'Chocolate'},
+                {flavor2: 'Vanilla'},
+                {flavor3: 'Strawberry'},
+                {flavor4: 'Cookies n Cream'},
+                {flavor5: 'Mint Chocolate Chip'},
+                {flavor6: 'Pistachio'},
+                {flavor7: 'Birthday Cake'},
+                {flavor8: 'Buttered Pecan'},
+            ],
+            holder: [
+                {holder1:'Cup'},
+                {holder2: 'Bowl'},
+                {holder3: 'Cone'},
+                {holder4: 'Waffle Cone'},
+            ],
+            toppings: [
+                {flavor1:'Oreos'},
+                {flavor2: 'Sprinkles'},
+                {flavor3: 'Gummy Bears'},
+                {flavor4: 'Peanuts'},
+                {flavor5: 'Peanut Butter Cups'},
+                {flavor6: 'Hot Fudge'},
+                {flavor7: 'Chocolate Sauce'},
+                {flavor8: 'Pecans'},
+            ],
+            size: [
+                {size1: 'Small'},
+                {size2: 'Medium'},
+                {size3: 'Large'},
+                {size4: 'Extra-Large'},
+            ]
         }
     ]
   
