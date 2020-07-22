59a3dd0a0938bb3d2837e5230dcdf0e15c9e083a
Joe Covarrubias <jocovarrubias@joes-air-2.home>
2020-07-22 09:02:10 -0700

    required true/false

--- models/milkshakeModels.js
+++ models/milkshakeModels.js
@@ -4,9 +4,9 @@ const milkshakeSchema = new mongoose.Schema(
     {
         name: { type: String, required: true},
         flavor: { type: String, required: true },
-        topping1: { type: String, required: true },
-        topping2: {type: String, required: true},
-        topping3: {type: String, required: true},
+        topping1: { type: String, required: false },
+        topping2: {type: String, required: false},
+        topping3: {type: String, required: false},
         size: { type: String, required: true},
         price: { type: Number, required: true},
         paid: { type: Boolean, required: true}
